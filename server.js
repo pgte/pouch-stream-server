a2c607bf71c6a48b2262f7476ac540c79fbf17fa
Pedro Teixeira <i@pgte.me>
2015-10-05 10:26:49 +0100

    server stream options fix

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+'use strict';
+
 var Stream = require('./stream');
 var DBs = require('./dbs');
+var extend = require('xtend');
 
 module.exports = Server;
 
-function Server(options) {
+function Server(serverOptions) {
   var dbs = DBs();
   return {
     dbs: dbs,
-    stream: function(options) {
-      return Stream(dbs, options);
-    }
+    stream: function createStream(options) {
+      return Stream(dbs, extend({}, serverOptions || {}, options));
+    },
   };
 }
